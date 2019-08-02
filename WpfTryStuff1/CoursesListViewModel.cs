ad7e7b46a2473967bd1da625341e4645ac15233b
NeverEndsLoop <NeverEndsLoop@gmail.com>
2019-08-02 15:28:57 +0300

    List to show data as work going on.

---
+++ WpfTryStuff1/CoursesListViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfTryStuff1
+{
+    public class CoursesListViewModel
+    {
+        public ObservableCollection<CourseItemViewModel> Items { get; set; }
+    }
+}
---
+++ WpfTryStuff1/CoursesListViewModelDesign.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UtillsCalAvrg;
+
+namespace WpfTryStuff1
+{
+    public class CoursesListViewModelDesign : CoursesListViewModel
+    {
+        public static CoursesListViewModelDesign Lists => new CoursesListViewModelDesign();
+
+        public CoursesListViewModelDesign()
+        {
+            Items = new ObservableCollection<CourseItemViewModel>()
+            {
+            new CourseItemViewModel() { CourseName = ".Net & C#"   ,Mark= 92,Points = 3     ,Year = 2,Semester = eSemester.B},
+            new CourseItemViewModel() { CourseName = "Graph theory", Mark = 80, Points = 3.5f, Year = 2, Semester = eSemester.A },
+            new CourseItemViewModel() { CourseName = "Linery", Mark = 61, Points = 5, Year = 1, Semester = eSemester.A },
+            new CourseItemViewModel() { CourseName = "C++", Mark = 99, Points = 5, Year = 2, Semester = eSemester.A },
+            new CourseItemViewModel() { CourseName = "Prog c", Mark = 79, Points = 5, Year = 1, Semester = eSemester.A },
+            new CourseItemViewModel() { CourseName = "C pro", Mark = 90, Points = 4f, Year = 1, Semester = eSemester.B },
+            new CourseItemViewModel() { CourseName = "Data Structs", Mark = 85, Points = 4f, Year = 1, Semester = eSemester.B },
+            new CourseItemViewModel() { CourseName = "Philosopy44", Mark = 50, Points = 2, Year = 1, Semester = eSemester.C },
+            new CourseItemViewModel() { CourseName = "Philosopy", Mark = 90, Points = 2, Year = 1, Semester = eSemester.C },
+            };
+
+           // fillData();
+        }
+
+        private void fillData()
+        {
+          
+
+
+        }
+
+
+    }
+}
+
